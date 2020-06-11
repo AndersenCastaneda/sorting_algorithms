31aaba3f2949db0c78044e2b01653bf2bcfd54d5
AndersenCastaneda <andersenpiano@gmail.com>
2020-06-11 22:13:45 +0000

    add bubble_sort()

---
+++ 0-bubble_sort.c
@@ -0,0 +1,24 @@
+#include "sort.h"
+
+void switch_value()
+{
+}
+
+/**
+ * bubble_sort - Sorts an array of integers in
+ * ascending order using the Bubble sort algorithm
+ * @array: Array to sort (head's pointer)
+ * @size: Array size
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+
+	if (!array || size == 0)
+		return;
+
+	do
+	{
+
+	} while (size <= 1);
+}
