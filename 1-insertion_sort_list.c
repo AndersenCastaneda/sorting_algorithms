0029ad0ffae8bf3a54b2a12f0d3cb0f6629515f0
AndersenCastaneda <andersenpiano@gmail.com>
2020-06-16 19:37:18 +0000

    Fix case 13

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,6 +2,23 @@
 #include <stdio.h>
 
 /**
+ * swap_nodes - Swap two nodes
+ * @first: first node
+ * @second: second node
+ */
+void swap_nodes(listint_t *first, listint_t *second)
+{
+	if (first->prev)
+		first->prev->next = second;
+	if (second->next)
+		second->next->prev = first;
+	first->next = second->next;
+	second->prev = first->prev;
+	first->prev = second;
+	second->next = first;
+}
+
+/**
  * insertion_sort_list - Sorts a doubly linked list of
  * integers in ascending order using the Insertion sort algorithm
  * @list: linked list to sort
@@ -9,7 +26,6 @@
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *index = *list, *aux;
-	int swap = 1;
 
 	if (!list || !index || !index->next)
 		return;
@@ -26,33 +42,9 @@ void insertion_sort_list(listint_t **list)
 				if (!aux->prev)
 					*list = aux;
 				print_list(*list);
-				if (swap == 1)
-				{
-					swap = 0;
-					if (index->next)
-						index = index->next;
-				}
 			}
 			else
 				aux = aux->prev;
 		}
-		swap = 1;
 	}
-}
-
-/**
- * swap_nodes - Swap two nodes
- * @previous: first node
- * @current: second node
- */
-void swap_nodes(listint_t *first, listint_t *second)
-{
-	if (first->prev)
-		first->prev->next = second;
-	if (second->next)
-		second->next->prev = first;
-	first->next = second->next;
-	second->prev = first->prev;
-	first->prev = second;
-	second->next = first;
 }
