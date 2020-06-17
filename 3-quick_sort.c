0920c80fdebc8cc9cf657c79a41f6643f4777991
AndersenCastaneda <andersenpiano@gmail.com>
2020-06-17 17:51:57 +0000

    Add task 3

---
+++ 3-quick_sort.c
@@ -0,0 +1,62 @@
+#include "sort.h"
+
+/**
+ * swap - Swap two values on an array
+ * @array: Array to swap
+ * @a: First index to swap
+ * @b: Second index to swap
+ */
+void swap(int *array, int a, int b)
+{
+	int temp = array[a];
+	array[a] = array[b];
+	array[b] = temp;
+}
+
+/**
+ * quick_recursion - Sorts using recursion
+ * @array: Array to sort
+ * @size: Size of array
+ * @start: Start index
+ * @end: End index
+ */
+void quick_recursion(int *array, int start, int end, size_t size)
+{
+	long int partitionIndex = start - 1, i;
+
+	if (start < end)
+	{
+		for (i = start; i < end; i++)
+		{
+			if (array[i] <= array[end])
+			{
+				partitionIndex++;
+				if (partitionIndex != i)
+				{
+					swap(array, partitionIndex, i);
+					print_array(array, size);
+				}
+			}
+		}
+		if (partitionIndex + 1 != end)
+		{
+			swap(array, partitionIndex + 1, end);
+			print_array(array, size);
+		}
+		quick_recursion(array, start, partitionIndex, size);
+		quick_recursion(array, partitionIndex + 2, end, size);
+	}
+}
+
+/**
+ * quick_sort - Sorts an array of integers in ascending
+ * order using the Quick sort algorithm
+ * @array: Array to sort
+ * @size: Size of array
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (!array || size < 2)
+		return;
+	quick_recursion(array, 0, size - 1, size);
+}
